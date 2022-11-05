fadd1be5ef2daa7c2c9648c65690284a51b1d24b
Narmina <naraibr22@gmail.com>
2022-11-06 01:48:30 +0400

    added lines on enter, inputs

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-document.querySelector('input').addEventListener('keyup', (event) =>{
-  if (event.key == "Enter") {
-    let newEl = document.createElement('div');
-    newEl.className = 'text';
-    newEl.textContent = event.target.value;
-    event.target.outerHTML = newEl.outerHTML;
-  }
-})
+function update () {
+  document.querySelectorAll('input').forEach(item => {
+    item.addEventListener('keyup', (event) =>{
+      if (event.key == "Enter") {
+        event.target.readOnly = true;
+        // let newEl = document.createElement('div');
+        // newEl.className = 'text';
+        // newEl.textContent = event.target.value;
+        // // console.log(event.target.outerHTML)
+        // // console.log(newEl.outerHTML)
+        // event.target.outerHTML = newEl.outerHTML;
+      }
+    })
+  })
+}
+
+update()
+
+let addButton = document.querySelector('.add-button');
+let container = document.querySelector('.container');
+
+addButton.addEventListener('click', (event) => {
+  let newLine = document.createElement('div')
+  newLine.className = 'line'
+  newLine.innerHTML = '<input type="text">\n<button class="delete"><img class="x" src="pic/X-grey.svg" alt="x"></button>'
+  container.append(newLine)
+  update()
+})    
