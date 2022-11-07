a935541c48e0ce4f4f39fdb04044e0caf407e4f1
Narmina <naraibr22@gmail.com>
2022-11-08 00:58:13 +0400

    added drag and drop, working on delete button

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// function update () {
-//   document.querySelectorAll('input').forEach(item => {
-//     item.addEventListener('keyup', (event) =>{
-//       if (event.key == "Enter") {
-//         event.target.readOnly = true;
-//       }
-//     })
-//   })
-// }
-
-// update()
-
-// let addButton = document.querySelector('.add-button');
-// let container = document.querySelector('.container');
-
-// addButton.addEventListener('click', (event) => {
-//   let newLine = document.createElement('div')
-//   newLine.className = 'line'
-//   newLine.innerHTML = '<input type="text">\n<button class="delete"><img class="x" src="pic/X-grey.svg" alt="x"><img class="x-violet" src="pic/X-violet.svg" alt="x"></button>'
-//   container.append(newLine)
-//   update()
-// })    
 let container = document.querySelector('.container');
 
 function newInput() { 
@@ -30,17 +8,24 @@ function newInput() {
 }
 
 function update () {
-  document.querySelectorAll('input').forEach(item => {
-    item.addEventListener('keyup', (event) =>{
+  document.querySelectorAll('input').forEach(item1 => {
+    item1.addEventListener('keyup', (event) =>{
       if (event.key == "Enter") {
         event.target.readOnly = true;
         newInput();
         update();
       }
+      document.querySelectorAll('.line').forEach(item => {
+        document.querySelectorAll('.delete').forEach(item2 => {
+          item2.addEventListener('click', () => {
+            if (item1.readOnly == false) document.querySelector('input').value = '';
+            else if (item1.readOnly == true) item.remove()
+          })
+        })
+      })
     })
   })
 }
-
 update();
 
 
@@ -51,8 +36,17 @@ addButton.addEventListener('click', (event) => {
   update();
 })    
 
-let remover = document.querySelector('.delete');
-remover.addEventListener('click', () => {
-  if (Input.target.readOnly == true) document.querySelector('.line').remove();
-  else document.querySelector('input').value = '';
-})
+let remover = document.querySelectorAll('.delete');
+
+// remover.forEach(item => {
+//   item.addEventListener('click', () => {
+//     document.querySelectorAll('input').forEach(item => {
+//       if (item.readOnly == false) document.querySelector('input').value = '';
+//       else if (item.readOnly == true) document.querySelector('.line').remove();
+//     })
+//   })
+// })
+
+
+let drag = document.querySelector('.container');
+new Sortable(drag, {group: { name: 'shared', pull: 'clone'}, animation: 150})
